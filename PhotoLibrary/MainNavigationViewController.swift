410e1f8b5ccdae2c7b9ac834804d47abb705dec0
bannzai <kingkong999yhirose@gmail.com>
2020-04-16 23:24:46 +0900

    :sparkles: add MainNavigationViewController

---
+++ PhotoLibrary/MainNavigationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainNavigationViewController.swift
+//  PhotoLibrary
+//
+//  Created by Yudai.Hirose on 2020/04/16.
+//  Copyright © 2020 bannzai. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
