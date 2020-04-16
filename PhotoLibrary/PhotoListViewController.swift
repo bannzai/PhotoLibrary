ea9796eb05aa45fd519162fd6d86d932bb9cf459
bannzai <kingkong999yhirose@gmail.com>
2020-04-16 23:23:54 +0900

    :sprakles: add photo list view controller

---
+++ PhotoLibrary/PhotoListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoListViewController.swift
+//  PhotoLibrary
+//
+//  Created by Yudai.Hirose on 2020/04/16.
+//  Copyright © 2020 bannzai. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
