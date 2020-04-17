e001c5bf75abd3b42a8c34ff2c43549f649b00e9
bannzai <kingkong999yhirose@gmail.com>
2020-04-17 10:20:04 +0900

    :art: add list cell

---
+++ PhotoLibrary/Photo/PhotoListCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoListCollectionViewCell.swift
+//  PhotoLibrary
+//
+//  Created by Yudai.Hirose on 2020/04/17.
+//  Copyright © 2020 bannzai. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
