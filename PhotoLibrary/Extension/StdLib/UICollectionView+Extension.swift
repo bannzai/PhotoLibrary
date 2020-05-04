8bd9735438eaf5448013c4ab7c3d23ebc28a0f07
bannzai <kingkong999yhirose@gmail.com>
2020-05-05 02:26:48 +0900

    :sparkels: add utility function for dequeue cell

--- PhotoLibrary/Extension/StdLib/UICollectionView+Extension.swift
+++ PhotoLibrary/Extension/StdLib/UICollectionView+Extension.swift
@@ -12,4 +12,7 @@ extension UICollectionView {
     func registerCell<T: CollectionViewCellType>(type: T.Type) {
         register(type.nib(), forCellWithReuseIdentifier: type.identifier)
     }
+    func dequeueReusableCell<T: CollectionViewCellType>(type: T.Type, indexPath: IndexPath) -> T {
+        dequeueReusableCell(withReuseIdentifier: type.identifier, for: indexPath) as! T
+    }
 }
